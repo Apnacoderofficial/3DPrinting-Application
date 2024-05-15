df876bff0de1abe2f70501d5820b3001e53b95aa
Yesveer <80391289+Yesveer@users.noreply.github.com>
2024-05-16 02:17:53 +0530

    Adding Installation File to This Project

---
+++ Step2.bat
@@ -0,0 +1,34 @@
+REM Clone a Git repository
+echo.
+echo Cloning a Git repository of Code...
+
+REM Define the repository URL
+set "REPO_URL=https://github.com/Apnacoderofficial/3DPrinting-Application.git"
+
+REM Define the directory where the repository will be cloned
+set "CLONE_DIR=C:\Program Files\3D sPrinting Application"
+
+REM Create the directory if it doesn't exist
+if not exist "%CLONE_DIR%" mkdir "%CLONE_DIR%"
+
+REM Change to the directory where the repository will be cloned
+cd /D "%CLONE_DIR%"
+
+REM Clone the repository
+git clone "%REPO_URL%"
+
+echo.
+echo Repository cloned successfully.
+
+REM Run the npm start to start the port 
+echo.
+echo Running the npm start...
+
+REM Define the directory where the repository will be cloned
+set "CLONE_DIR=C:\Program Files\3D sPrinting Application\3DPrinting-Application"
+cd /D "%CLONE_DIR%"
+npm install 
+npm start
+
+echo.
+echo Installation completed.
