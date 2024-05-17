997a72c99dd56c7efd274423f5ea48baab9a8cd5
Rishabh Srivastava <117577787+errishabhsrivastava@users.noreply.github.com>
2024-05-17 11:17:42 +0530

    3dPrinting v1.0.8

--- Step1.bat
+++ Step1.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: Check for administrative privileges
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+
 REM Node.js installation
 echo Installing Node.js...
 
